a37eb00c6303efa8a2ed0a88a7ee581affd6e4dd
Rachel Harris <rachel.harris@msn.com>
2021-12-25 12:58:24 +1100

    Setup page to only show/hide content before starting game, add initial timer

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -8,3 +8,102 @@
 // THEN the game is over
 // WHEN the game is over
 // THEN I can save my initials and my score
+
+// Sections on the page to show/hide based on stage of game
+var questionSection = document.getElementById("questions").hidden = true;
+var scoresSection = document.getElementById("scores").hidden = true;
+var scoreboardSection = document.getElementById("scoreboard").hidden = true;
+var startGameSection = document.getElementById("start-game")
+var startButton = document.getElementById("start-button")
+
+var timer = document.getElementById("timer")
+var secondsRemaining = 10;
+
+
+// Do I need any of this? Should I use setAttribute instead?
+// var questionTitle1 = document.getElementById('#question-title');
+// var answerA1 = document.getElementById('#answerA');
+// var answerB1 = document.getElementById('#answerB');
+// var answerC1 = document.getElementById('#answerC');
+// var answerD1 = document.getElementById('#answerD');
+
+var questions = [ 
+    {
+        questionTitle: "Which is Australia's second largest state by area?",
+        answerA: "New South Wales", 
+        answerB: "Queensland (correct answer)", 
+        answerC: "South Australia",
+        answerD: "Western Australia", 
+    },
+    {
+        questionTitle: "Which is the largest desert in Australia?",
+        answerA: "Gibson Desert", 
+        answerB: "Great Sandy Desert", 
+        answerC: "Great Victoria Desert (correct answer)",
+        answerD: "Simpson Desert", 
+    },
+    {
+        questionTitle: "Which body of water is home to the Great Barrier Reef?",
+        answerA: "Arafura Sea", 
+        answerB: "Coral sea (correct answer)", 
+        answerC: "Solomon Sea",
+        answerD: "Timor sea", 
+    },
+    {
+        questionTitle: "The Ashmore and Cartier Islands are situated in which ocean?",
+        answerA: "Atlantic Ocean", 
+        answerB: "Indian Ocean (correct answer)", 
+        answerC: "Pacific Ocean",
+        answerD: "Southern Ocean", 
+    },
+    {
+        questionTitle: "Which is the most populous of the Torres Strait Islands?",
+        answerA: "Badu Island", 
+        answerB: "Horn Island", 
+        answerC: "Murray Island",
+        answerD: "Thursday Island (correct answer)", 
+    },
+  ];
+
+// Timer function
+function startTimer() {
+    var TimerInterval = setInterval(function() {
+        secondsRemaining--;
+        timer.textContent = secondsRemaining;
+
+        if(secondsLeft === 0) {
+            clearInterval(timerInterval);
+        }
+        // Incriment or decriment score
+            // timeleft = timeleft - 10
+            // Use minus equals to reduce the time
+        // Show next question
+    }, 1000);
+}
+
+startButton.addEventListener("click", startTimer());
+
+function startGame() {
+    questionSection.setAttribute("hidden", false);
+    startGameSection.setAttribute("hidden", true);
+    // start the timer
+  }
+
+startButton.addEventListener("click", startGame());
+
+
+
+
+
+// Show the question
+
+// function hideScores() {
+    
+// }
+
+
+// function startTimer() {
+
+// }
+
+// use minus equals to reduce the time
